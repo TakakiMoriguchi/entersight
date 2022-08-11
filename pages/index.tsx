687619f8e2ce35bd5f553d6d440641903fc44503
Reimei Rigel <sara_rigel@icloud.com>
2022-08-11 03:23:17 +0000

    0811

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import style from '../styles/home.module.scss'
 import OurServices from '../components/home/OurServices'
 import PartnersCompany from '../components/home/PartnersCompany'
 import SkillArea from '../components/home/SkillArea'
+import BlogArea from '../components/home/BlogArea'
 
 import { Box, Container } from '@chakra-ui/react'
 
@@ -23,9 +24,12 @@ export default function Home() {
           <hr />
 
           <ul>
-            <li>tech blog</li>
             <li>service(tinywest/8beat)</li>
-            <li>blog</li>
+            
+
+            <BlogArea />
+
+            
             <li>partner</li>
           </ul>
 
