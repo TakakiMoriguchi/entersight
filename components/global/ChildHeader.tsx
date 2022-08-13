4feb182c18bc40707821a28f6f3b34136aa9f4f9
TakakiMoriguchi <entersight@gmail.com>
2022-08-13 08:54:16 +0000

    works page created

--- components/global/ChildHeader.tsx
+++ components/global/ChildHeader.tsx
@@ -1,7 +1,13 @@
+import {
+  Container,
+  Box
+} from '@chakra-ui/react'
+import Logo from '../../public/images/global/logo_yoko.svg'
+
 export default function ChildHeader() {
   return(
     <>
-      childHeader
+      ENTERSIGHT
     </>
   )
 }
